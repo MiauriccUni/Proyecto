6d6a353e76dfe4944e90f60a13ffd0b37f40a469
jvillegasv <165696672+jvillegasv@users.noreply.github.com>
2024-07-25 16:23:46 -0600

    Nuevo UsuarioUpdateController API y UserUpdate DTO

---
+++ DTO/UserUpdate.cs
@@ -0,0 +1,14 @@
+﻿namespace DTO
+{
+    public class UsuarioDTO : BaseClass
+    {
+        public string Correo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public string Celular { get; set; }
+        public string Contrasenna { get; set; }
+        public string Rol { get; set; }
+        public string Genero { get; set; }
+    }
+}
