447b4f1b892edeb4c097c47f5285b64a795a1bf4
Laureen Tellez Zamora <151061783+laureenTellezz@users.noreply.github.com>
2024-07-27 08:24:07 -0600

    Add files via upload

---
+++ DTO/RegistroRutina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class RegistroRutina : BaseClass
+    {
+        public int Repeticiones { get; set; }
+        public int Peso { get; set; }
+        public int Series { get; set; }
+        public DateTime tiempo { get; set; }
+
+    }
+}
---
+++ DTO/rutinas.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO
+{
+    public class rutinas:BaseClass
+    {
+        public string NombreEjercicio { get; set; }
+        public string TipoEjercicio { get; set; }
+
+    }
+}
