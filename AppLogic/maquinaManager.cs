58d499fd34945440d1a82745903db5d3e57d96fb
Laureen Tellez Zamora <151061783+laureenTellezz@users.noreply.github.com>
2024-07-27 08:04:00 -0600

    Add files via upload

---
+++ AppLogic/maquinaManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Crud;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public class maquinaManager
+    {
+        public string Createmaquina(maquina maquinas)
+        {
+           maquinaCrud crud = new maquinaCrud();
+            crud.Create(maquinas);
+            return "Ok";
+        }
+        public List<maquina> GetAllmaquina()
+        {
+            maquinaCrud crud = new maquinaCrud();
+            return crud.RetrieveAll<maquina>();
+        }
+        public List<maquina> GetRetrieveNombreMaquina(string NombreMaquina)
+        {
+            maquinaCrud maquinasCrud = new maquinaCrud();
+            return maquinasCrud.RetrieveBynombre_maquina<maquina>(NombreMaquina);
+        }
+
+      
+    }
+}
