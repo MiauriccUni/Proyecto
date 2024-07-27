e390ecd75f1a06a2bb343dc0719532084a916e9d
Laureen Tellez Zamora <151061783+laureenTellezz@users.noreply.github.com>
2024-07-27 08:12:21 -0600

    Add files via upload

---
+++ DataAccess/Crud/RutinaCrud.cs
@@ -0,0 +1,109 @@
+﻿using DataAccess.DAO;
+using DataAccess.Mappers;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Crud
+{
+    public class RutinaCrud:CrudFactory
+    {
+        RutinasMapper rutinasMapper;
+        public RutinaCrud() : base()
+
+        {
+            rutinasMapper = new RutinasMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseClass dto)
+        {
+            SqlOperation operation = rutinasMapper.GetCreateStatement(dto);
+            dao.ExecuteStoreProcedure(operation);
+        }
+
+        public override void Delete(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            List<T> resultList = new List<T>();
+            SqlOperation operation = rutinasMapper.GetRetrieveAllStatement();
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+            if (dataResults.Count > 0)
+            {
+                var dtoList = rutinasMapper.BuildObjects(dataResults);
+                foreach (var dto in dtoList)
+                {
+                    resultList.Add((T)Convert.ChangeType(dto, typeof(T)));
+                }
+            }
+            return resultList;
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            //List<T> result = new List<T>();
+            //SqlOperation operation = usuarioMapper.GetRetrieveByIdStatement(id);
+
+            //List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            //if(dataResults.Count > 0)
+            //{
+            //    var dtoList = usuarioMapper.BuildObjects(dataResults);
+            //    foreach(var dto in dtoList)
+            //    {
+            //        result
+            //    }
+            //}
+            throw new NotImplementedException();
+        }
+        public override void Update(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> RetrieveBynombre_ejercicio<T>(string NombreEjercicio)
+        {
+            List<T> resultList = new List<T>();
+            SqlOperation operation = rutinasMapper.GetRetrieveBynombre_ejercicio(NombreEjercicio);
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            if (dataResults.Count > 0)
+            {
+                var dtoList = rutinasMapper.BuildObjects(dataResults);
+                foreach (var dto in dtoList)
+                {
+                    resultList.Add((T)Convert.ChangeType(dto, typeof(T)));
+                }
+            }  
+            return resultList;
+
+        }
+        public List<T> RetrieveBytipos_de_ejercicio<T>(string TiposEjercicio)
+        {
+            List<T> resultList = new List<T>();
+            SqlOperation operation = rutinasMapper.GetRetrieveBytipo_de_ejercicio(TiposEjercicio);
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            if (dataResults.Count > 0)
+            {
+                var dtoList = rutinasMapper.BuildObjects(dataResults);
+                foreach (var dto in dtoList)
+                {
+                    resultList.Add((T)Convert.ChangeType(dto, typeof(T)));
+                }
+            }
+            return resultList;
+
+        }
+    }
+}
---
+++ DataAccess/Crud/maquinaCrud.cs
@@ -0,0 +1,92 @@
+﻿using DataAccess.DAO;
+using DataAccess.Mappers;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Crud
+{
+    public class maquinaCrud : CrudFactory
+    {
+        maquinaMapper maquinaMappers;
+        public maquinaCrud () : base ()
+        
+        {
+            maquinaMappers = new maquinaMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseClass dto)
+        {
+            SqlOperation operation = maquinaMappers.GetCreateStatement(dto);
+            dao.ExecuteStoreProcedure(operation);
+        }
+
+        public override void Delete(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            List<T> resultList = new List<T>();
+            SqlOperation operation = maquinaMappers.GetRetrieveAllStatement();
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+            if (dataResults.Count > 0)
+            {
+                var dtoList = maquinaMappers.BuildObjects(dataResults);
+                foreach (var dto in dtoList)
+                {
+                    resultList.Add((T)Convert.ChangeType(dto, typeof(T)));
+                }
+            }
+            return resultList;
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            //List<T> result = new List<T>();
+            //SqlOperation operation = usuarioMapper.GetRetrieveByIdStatement(id);
+
+            //List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            //if(dataResults.Count > 0)
+            //{
+            //    var dtoList = usuarioMapper.BuildObjects(dataResults);
+            //    foreach(var dto in dtoList)
+            //    {
+            //        result
+            //    }
+            //}
+            throw new NotImplementedException();
+        }
+        public override void Update(BaseClass dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> RetrieveBynombre_maquina<T>(string nombre_maquina)
+        {
+            List<T> resultList = new List<T>();
+            SqlOperation operation = maquinaMappers.GetRetrieveBynombre_maquina(nombre_maquina);
+
+            List<Dictionary<string, object>> dataResults = dao.ExecuteStoredProcedureWithQuery(operation);
+
+            if (dataResults.Count > 0)
+            {
+                var dtoList = maquinaMappers.BuildObjects(dataResults);
+                foreach (var dto in dtoList)
+                {
+                    resultList.Add((T)Convert.ChangeType(dto, typeof(T)));
+                }
+            }
+            return resultList;
+
+        }
+      
+    }
+}
