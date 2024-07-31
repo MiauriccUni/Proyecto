07a6b45d41705c8c5ede91fa8dd01a1318018259
jcenfo93 <jsanchezs3@ucenfotec.ac.cr>
2024-07-31 12:18:45 -0600

    Update link de vistas y nuevos paneles de entrenador y recepcionista

--- WEB-UI/Controllers/EntrenadorController.cs
+++ WEB-UI/Controllers/EntrenadorController.cs
@@ -3,8 +3,24 @@
 namespace WEB_UI.Controllers
 {
     public class EntrenadorController : Controller
+
     {
+        public IActionResult PanelEntrenador()
+        {
+            return View();
+        }
+
         public IActionResult Entrenador()
+        {
+            return View();
+        }
+
+        public IActionResult MaquinasEntrenador()
+        {
+            return View();
+        }
+
+        public IActionResult Rutinas()
         {
             return View();
         }
