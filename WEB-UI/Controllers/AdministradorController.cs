e25102b644eef78e83540ffce64fdb931ec8dffb
jcenfo93 <jsanchezs3@ucenfotec.ac.cr>
2024-07-30 22:28:08 -0600

    Separación de vistas en carpetas

---
+++ WEB-UI/Controllers/AdministradorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult AdminPanelPrincipal()
+        {
+            return View();
+        }
+        public IActionResult AdminActualizacionUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult AdminRecuperacionContrasenna()
+        {
+            return View();
+        }
+
+        public IActionResult ActualizarDatosUsuarios()
+        {
+            return View();
+        }
+
+        public IActionResult MaquinasAdministrador()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEB-UI/Controllers/EntrenadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class EntrenadorController : Controller
+    {
+        public IActionResult Entrenador()
+        {
+            return View();
+        }
+    }
+}
