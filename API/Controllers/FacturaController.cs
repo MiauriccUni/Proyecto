cde3001f6bc55056ae29014b417500a3f9ed7e23
jcenfo93 <jsanchezs3@ucenfotec.ac.cr>
2024-07-31 22:31:13 -0600

    Inicios de Factura

---
+++ API/Controllers/FacturaController.cs
@@ -0,0 +1,17 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [EnableCors("Demo_Policy")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FacturaController : ControllerBase
+    {
+        [HttpPost]
+        public string CrearFactura(Factura factura) { 
+            
+        }
+    }
+}
---
+++ DTO/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace DTO
+{
+    public class Factura : BaseClass
+    {
+        public DateTime FechaPago { get; set; }
+
+        public decimal Descuento { get; set; }
+        public decimal MontoFinal { get; set; }
+
+    }
+}
