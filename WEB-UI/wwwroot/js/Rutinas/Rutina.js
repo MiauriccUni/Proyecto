9cf4a6557f4f2f67be50ee375909f38e2e2378cf
ltellezz <ltellezz@ucenfotec.ac.cr>
2024-08-01 22:08:07 -0600

    Se agregreagan Rutinas

--- WEB-UI/wwwroot/js/Rutinas/Rutina.js
+++ WEB-UI/wwwroot/js/Rutinas/Rutina.js
@@ -1,18 +1,16 @@
 ﻿function AgregarRutina() {
-    this.InitView = function{
-        $('#Rutina').click(function (event)){
+    this.InitView = function(){
+        $('#Rutinas').click(function (event){
             var view = new AgregarRutina();
             view.SubitRutinas();
-        }
-    });
-
+        });
+    }
     this.SubitRutinas = function () {
-        var rutinas = {}
-        rutinas.id = gereteUniqueID();
-        rutinas.nombreEjercicio = $('#AgregarNombre').val();
-        rutinas.tipoEjercicio = $('AgregarTipoEjer').val();
-
-        if (rutinas.nombreEjercicio === "") {
+        var rutina = {}
+        rutina.id = generateUniqueId();
+        rutina.nombreEjercicio = $('#AgregarNombre').val();
+        rutina.tipoEjercicio = $('#AgregarTipoEjer').val();
+        if (rutina.nombreEjercicio === "") {
             Swal.fire({
                 icon: 'error',
                 text: "Por favor indique el nombre ejercicio.",
@@ -20,8 +18,7 @@
             });
             return;
         }
-
-        if (rutinas.tipoEjercicio === "") {
+        if (rutina.tipoEjercicio === "") {
             Swal.fire({
                 icon: 'error',
                 text: "Por favor indique el  tipo del ejercicio.",
@@ -29,7 +26,6 @@
             });
             return;
         }
-
         $.ajax({
             headers: {
                 'Accept': "application/json",
@@ -39,8 +35,7 @@
             url: "https://localhost:7253/api/Rutina/CreateRutina",
             contentType: "application/json;charset=utf-8",
             dataType: "json",
-            data: JSON.stringify(rutinas),
-            hasContent: true
+            data: JSON.stringify(rutina)
         }).done(function (result) {
             Swal.fire({
                 title: "Éxito",
@@ -54,24 +49,21 @@
                 title: 'Error',
             });
         });
-
-        generatedIds = [];
-
-        generateUniqueId = () => {
-            let newId;
-            do {
-                const randomNumber = Math.floor(100000 + Math.random() * 900000);
-                newId = parseInt(randomNumber);
-            } while (generatedIds.includes(newId));
-            generatedIds.push(newId);
-            return newId;
-        }
-
-        $(document).ready(function () {
-            var view = new CrearCitaMedicion();
-            view.InitView();
-        });
-
-      
     }
+    
 }
+generatedIds = [];
+generateUniqueId = () => {
+    let newId;
+    do {
+        const randomNumber = Math.floor(100000 + Math.random() * 900000);
+        newId = parseInt(randomNumber);
+    } while (generatedIds.includes(newId));
+    generatedIds.push(newId);
+    return newId;
+}
+
+$(document).ready(function () {
+    var view = new AgregarRutina();
+    view.InitView();
+});
