980734b7d96693aef5662f3b7ba258256a4b1a2f
ltellezz <ltellezz@ucenfotec.ac.cr>
2024-08-03 06:44:49 -0600

    se agrega a view de Rutinas el get all para la vista del usuario

---
+++ WEB-UI/wwwroot/js/Rutinas/RutinaU.js
@@ -0,0 +1,22 @@
+﻿function Consultar() {
+    const grid = new gridjs.Grid({
+        search: true,
+        sort: true,
+        resizable: true,
+        pagination: {
+            limit: 3
+        },
+        columns: ['Nombre del ejercicio', 'Tipo de ejercicio'],
+        server: {
+            url: 'https://localhost:7253/api/Rutina/Getrutina',
+            then: data => data.map(result => [
+                result.nombreEjercicio,
+                result.tipoEjercicio,
+            ])
+        },
+    }).render(document.getElementById('myGrid'));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    Consultar();
+});
