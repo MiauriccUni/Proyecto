de6e71242598dc8233f9ee8e00439e04d79ea917
jvillegasv <165696672+jvillegasv@users.noreply.github.com>
2024-08-06 23:00:57 -0600

    API Clases Grupales

---
+++ DTO/ClasesGrupales.cs
@@ -0,0 +1,13 @@
+﻿namespace DTO
+{
+    public class ClasesGrupales : BaseClass
+    {
+        public string NombreClase { get; set; }
+        public DateTime Horarios { get; set; }
+        public int CuposDisponibles { get; set; }
+    }
+}
+
+
+//comentario rambo
+
