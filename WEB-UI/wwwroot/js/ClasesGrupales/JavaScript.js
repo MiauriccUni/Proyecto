9e78ffa634f2c66fb472961e31fb088e01050c49
jvillegasv <165696672+jvillegasv@users.noreply.github.com>
2024-08-07 23:17:24 -0600

    Se pueden listar las Clases en el View

---
+++ WEB-UI/wwwroot/js/ClasesGrupales/JavaScript.js
@@ -0,0 +1,43 @@
+﻿function Consultar() {
+    const grid = new gridjs.Grid({
+        search: true,
+        sort: true,
+        resizable: true,
+        pagination: {
+            limit: 5
+        },
+        language: {
+            search: {
+                placeholder: 'Buscar'
+            }
+        },
+        columns: ['id', 'nombreClase', 'horarios', 'cuposDisponibles'],
+        server: {
+            url: 'https://localhost:7253/api/ClasesGrupales/GetAllClasesGrupales',
+            then: data => data.data.map(result => [
+                result.nombreClase,
+                result.horarios,
+                result.cuposDisponibles,
+                result.id,
+            ])
+        },
+    }).render(document.getElementById('myGrid'));
+}
+
+generatedIds = [];
+
+generateUniqueId = () => {
+    let newId;
+    do {
+        const randomNumber = Math.floor(100000 + Math.random() * 900000);
+        newId = parseInt(randomNumber);
+    } while (generatedIds.includes(newId));
+    generatedIds.push(newId);
+    return newId;
+}
+
+$(document).ready(function () {
+    Consultar();
+    /*  var view = new CrearCitaMedicion();*/
+    view.InitView();
+});
