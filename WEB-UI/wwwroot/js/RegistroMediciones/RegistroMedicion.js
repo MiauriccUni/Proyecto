14155185d982c84806bf342b2d051974242a5202
mussis <rvalverded@ucenfotec.ac.cr>
2024-08-12 22:50:34 -0600

    finalización de las validaciones

--- WEB-UI/wwwroot/js/RegistroMediciones/RegistroMedicion.js
+++ WEB-UI/wwwroot/js/RegistroMediciones/RegistroMedicion.js
@@ -18,30 +18,203 @@
         regMedicion.medicionPier = $('#Cintura').val();
         regMedicion.idCitaAsignacion = $('#idCita').val();
 
-        $.ajax({
-            headers: {
-                'Accept': "application/json",
-                'Content-Type': "application/json"
-            },
-            method: "POST",
-            url: "https://localhost:7253/api/Mediciones/CreateMediciones",
-            contentType: "application/json;charset=utf-8",
-            dataType: "json",
-            data: JSON.stringify(regMedicion),
-            hasContent: true
-        }).done(function (result) {
-            Swal.fire({
-                title: "Éxito",
-                icon: "success",
-                text: "Se ha completado el registro de las mediciones",
-            });
-
-        }).fail(function (error) {
+        if (regMedicion.peso === "") {
             Swal.fire({
                 icon: 'error',
-                text: "Error al registrar las mediciones",
-                title: 'Error',
+                text: "Por favor llene el espacio de Peso en Kilogramos.",
+                title: 'Error'
             });
+            return;
+        }
+
+        if (regMedicion.estatura === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de Estatura en Metros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.porcentageG === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de Porcentage de Grasa.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.notas === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de Notas.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionEs === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de medición de Espalda en Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionCin === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de medición de Cintura en Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionPier === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de medición de pierna en Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.idCitaAsignacion === "") {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor llene el espacio de identificador de la cita.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.peso <= 30) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique un peso no menor o igual a 30 Kilogramos.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.peso >= 180) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique un peso no mayor o igual a 180 Kilogramos.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionEs >= 180) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 180 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionCin >= 180) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 180 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionPier >= 180) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 180 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionEs <= 10) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 10 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionCin <= 10) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 10 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        if (regMedicion.medicionPier <= 10) {
+            Swal.fire({
+                icon: 'error',
+                text: "Por favor indique una medida no mayor o igual a 10 Centimetros.",
+                title: 'Error'
+            });
+            return;
+        }
+
+        iddecita = $('#idCita').val();
+
+        $.ajax({
+            url: "https://localhost:7253/api/Mediciones/GetMediciones",
+            method: "GET",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json"
+        }).done(function (result) {
+
+            result.forEach((obj, index) => {
+                if (obj.idCitaAsignacion == iddecita) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error",
+                        text: "El número de cita ya se encuentra registrado"
+                    });
+                } else if (obj.idCitaAsignacion != iddecita) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error",
+                        text: "El número de cita indicado no corresponde a ninguno registrado"
+                    });
+                }
+                else if (obj.idCitaAsignacion != iddecita) {
+                    alert("Deberia registrar")
+                    $.ajax({
+                        headers: {
+                            'Accept': "application/json",
+                            'Content-Type': "application/json"
+                        },
+                        method: "POST",
+                        url: "https://localhost:7253/api/Mediciones/CreateMediciones",
+                        contentType: "application/json;charset=utf-8",
+                        dataType: "json",
+                        data: JSON.stringify(regMedicion),
+                        hasContent: true
+                    }).done(function (result) {
+                        Swal.fire({
+                            title: "Éxito",
+                            icon: "success",
+                            text: "Se ha completado el registro de las mediciones",
+                        });
+
+                    }).fail(function (error) {
+                        Swal.fire({
+                            icon: 'error',
+                            text: "Error al registrar las mediciones",
+                            title: 'Error',
+                        });
+                    })
+                }
+            });
+
         })
     }
 
