e9308b2d67003ed9646c4cc8a83d1f0794f3c0dd
ltellezz <ltellezz@ucenfotec.ac.cr>
2024-08-13 18:56:50 -0600

    ..

--- WEB-UI/wwwroot/js/Rutinas/RutinaU.js
+++ WEB-UI/wwwroot/js/Rutinas/RutinaU.js
@@ -1,24 +1,2 @@
-﻿function Consultar() {
-    const grid = new gridjs.Grid({
-        search: true,
-        sort: true,
-        resizable: true,
-        pagination: {
-            limit: 5
-        },
-        columns: ['Nombre del ejercicio', 'Tipo de ejercicio', 'Repeticiones', 'Serires'],
-        server: {
-            url: 'https://localhost:7253/api/Rutina/Getrutina',
-            then: data => data.map(result => [
-                result.nombreEjercicio,
-                result.tipoEjercicio,
-                result.repeticiones,
-                result.series,
-            ])
-        },
-    }).render(document.getElementById('myGrid'));
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    Consultar();
-});  
+﻿const username = sessionStorage.getItem('correo');
+console.log(username);
