33c7f06181b8b71a075927f7b1682cbbf9d685ee
jvillegasv <165696672+jvillegasv@users.noreply.github.com>
2024-08-15 12:36:59 -0600

    Sin mayores cambios, sigue con error al conectar con la BD

--- DTO/ClasesGrupales.cs
+++ DTO/ClasesGrupales.cs
@@ -2,7 +2,6 @@
 {
     public class ClasesGrupales : BaseClass
     {
-        //public int IdClase { get; set; }
         public string NombreClase { get; set; }
         public DateTime Horarios { get; set; }
         public int CuposDisponibles { get; set; }
