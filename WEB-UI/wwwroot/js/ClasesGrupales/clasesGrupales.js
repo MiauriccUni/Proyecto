716f66e0eb17bd29d4a662f5b2fb5268917537c8
jvillegasv <165696672+jvillegasv@users.noreply.github.com>
2024-08-14 20:07:34 -0600

    botones de matricula

--- WEB-UI/wwwroot/js/ClasesGrupales/clasesGrupales.js
+++ WEB-UI/wwwroot/js/ClasesGrupales/clasesGrupales.js
@@ -86,15 +86,24 @@ function Consultar() {
             search: {
                 placeholder: 'Buscar'
             },
-        
-        pagination: {
-            previous: 'Anterior',
-            next: 'Siguiente',
-            showing: 'Mostrando',
-            results: () => 'resultados'
+            pagination: {
+                previous: 'Anterior',
+                next: 'Siguiente',
+                showing: 'Mostrando',
+                results: () => 'resultados'
             }
         },
-        columns: ['Nombre de la Clase', 'Horarios', 'Cupos Disponibles',],
+        columns: [
+            'Nombre de la Clase',
+            'Horarios',
+            'Cupos Disponibles',
+            {
+                name: 'Matricular',
+                formatter: (cell, row) => {
+                    return gridjs.html(`<button class="btn btn-primary" onclick="takeSpace(${row[3]})">Tomar un espacio</button>`);
+                }
+            }
+        ],
         headerVisible: false,
         server: {
             url: 'https://localhost:7253/api/ClasesGrupales/GetAllClasesGrupales',
